25622fee40ffea84bbd5e480a3e0ef4a4522b1d9
chmaxims <max.chesn@gmail.com>
2025-09-30 18:47:26 +0300

    minor changes

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_basket(browser):
+def test_visibility_of_basket(browser):
     browser.get(url)
     book = WebDriverWait(browser, 15).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket')))
 
