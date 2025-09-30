92d0133ff9585849572f64be75fd10a584f30aaf
chmaxims <max.chesn@gmail.com>
2025-09-30 18:31:48 +0300

    add test_items.py, change conftest.py

--- conftest.py
+++ conftest.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=es,
+    parser.addoption('--language', action='store', default='ru',
                      help="Choose language:es, fr, etc.")
 
-pytest.fixture(scope="function")
+@pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
     options = Options()
---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+
+url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_item(browser):
+    browser.get(url)
+    WebDriverWait(browser, 15).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket')))
+
+
+
+
