29796ebdb695d53da1f451cc3ce384aa32ff7c03
chmaxims <98893234+chmaxims@users.noreply.github.com>
2025-09-30 21:30:51 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_visibility_of_basket(browser):
     browser.get(url)
-    book = WebDriverWait(browser, 15).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket')))
+    book = WebDriverWait(browser, 15).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket')), message="Кнопка добавления в корзину отсутствует")
 
     assert book is not None
 
